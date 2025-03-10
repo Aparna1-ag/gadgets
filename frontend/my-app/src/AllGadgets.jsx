0447e9cf017b88359be02cc3e5e5b2047f98e787
Aparna Gupta <aparnagupta.csa@gmail.com>
2025-03-10 23:38:16 +0530

    added cards and info

--- frontend/my-app/src/AllGadgets.jsx
+++ frontend/my-app/src/AllGadgets.jsx
@@ -126,10 +126,10 @@ const AllGadgets = () => {
 
 
                 <div className='relative'>
-                    <img src='https://techcircuitworld.com/wp-content/uploads/2024/08/istockphoto-1497558248-612x612-2.webp' className='absolute top-0 left-0 w-full custom-bg-image' />
+                    {/* <img src='https://techcircuitworld.com/wp-content/uploads/2024/08/istockphoto-1497558248-612x612-2.webp' className='absolute top-0 left-0 w-full custom-bg-image' /> */}
 
 
-                    <div className='container absolute top-0 left-0 w-full flex justify-center'   >
+                    <div className='w-full absolute top-0 left-0 custom-bg-image'>
                 <div className='row mx-auto' style={{width: '90%'}}>
 
                     {
@@ -138,14 +138,84 @@ const AllGadgets = () => {
 
 
 
-                                <div className='col-md-4'   >
+                                <div className='col-md-6 mb-4'   >
 
 
-                                    <div className='mt-3 w-100 gadget-cards'>
-                                        <div>  {item.gadgetName}  </div>
-                                        <div> {item.brand}  </div>
-                                        <div>   {item.model}  </div>
+                                    <div className='gadget-cards '>
+
+                                   <div className='flex justify-between'>
+
+                                   <div>
+                                      <div className='text-xl exo-text'>   
+                                         <span> {item.brand}  </span>
+                                      <span>   {item.model}  </span> 
+                                       </div>
+
+                                       <div className='mt-3'> Resale Price: <span className='font-bold'>  {item.resalePrice}$</span></div>
+
+
+                                      <div> Seller: {item.salerName}
+
+                                      </div>
+                                      <div className='mt-5'>  Original Price: {item.originalPrice}$ 
+                                          </div>
+
+                                      <div>Posted on: <span className='font-bold'> {item.datePosted}</span>
+
+                                  
+
+
+                                      </div>
+                                      
+                                        </div>
+
+
+                                        {/* <img src="https://store.storeimages.cdn-apple.com/4668/as-images.apple.com/is/MQTR3?wid=1144&hei=1144&fmt=jpeg&qlt=90&.v=1687660671097" className='w-40 h-40' /> */}
+
+                                        <div id="carouselExampleIndicators" class="carousel slide w-48 h-48" data-bs-ride="carousel">
+  <div class="carousel-indicators">
+    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+  </div>
+  <div class="carousel-inner">
+    <div class="carousel-item active ">
+      <img src="https://store.storeimages.cdn-apple.com/4668/as-images.apple.com/is/MQTR3?wid=1144&hei=1144&fmt=jpeg&qlt=90&.v=1687660671097" class="d-block w-100" alt="..." />
+    </div>
+    <div class="carousel-item">
+      <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTGZdex_bY3K7zG3xjoayc_k2VfZPaiPXu11SqgKdSmt2YHiAN2ahvksBIC9ScxAiRcbtk&usqp=CAU" class="d-block w-100" alt="..." />
+    </div>
+    <div class="carousel-item">
+      <img src="https://static.bhphoto.com/images/multiple_images/images500x500/1389748872_IMG_364121.jpg" class="d-block w-100" alt="..." />
+    </div>
+  </div>
+  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Previous</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Next</span>
+  </button>
+</div>
+                                   
+                                   
+                                      
+
+
                                     </div>
+
+
+
+                                    <div className='flex justify-between w-3/4 mx-auto mt-5'>
+                                     <button className=' custom-btn btn-green mr-4' data-bs-toggle="modal" data-bs-target="#contact-info-modal"> <i class="fa-solid fa-address-card mr-1"></i> Contact Seller</button>
+                                     <button className='custom-btn btn-pink '> <i class="fa-solid fa-forward mr-1"></i>  More Details</button>
+                                        </div>
+                                    
+                                    
+                                    </div>
+
+                                   
 
                                 </div>
 
@@ -164,6 +234,26 @@ const AllGadgets = () => {
 
 
             </div>
+
+
+
+            <div class="modal fade" id="contact-info-modal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog modal-dialog-centered">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title " style={{color: "#192035"}} id="exampleModalLabel">Contact's Contact Info</h5>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+        <div  className='mt-1'>Email: <span className='font-bold'>abc@email.com</span></div>
+        <div className='mt-3'>Phone: <span className='font-bold'>9964645363</span></div>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary btn-sm bg-gradient" data-bs-dismiss="modal" style={{background: "#192035"}}>Close</button>
+      </div>
+    </div>
+  </div>
+</div>
           
 
         </div>
