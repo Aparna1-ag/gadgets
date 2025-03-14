57eace36f262d068490a69e3a9f08859d4f594c0
Aparna Gupta <aparnagupta.csa@gmail.com>
2025-03-15 04:02:34 +0530

    updates

--- frontend/my-app/src/App.jsx
+++ frontend/my-app/src/App.jsx
@@ -7,6 +7,7 @@ import {BrowserRouter as Router, Routes, Route} from "react-router"
 import AllGadgets from './AllGadgets';
 import Home from './Home';
 import GadgetDetails from './GadgetDetails';
+import Demo from './demoCloud';
 
 
 
@@ -23,6 +24,7 @@ function App() {
     <Route path='/register' element={<MyForm />}  />
     <Route path='/allgadgets' element={<AllGadgets />} />
     <Route path='/gadgetdetails/:id' element={<GadgetDetails />} />
+    <Route path='/demo'  element={<Demo/>} />
   </Routes>
 
 
